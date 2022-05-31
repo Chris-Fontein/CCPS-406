63fed8fe5eb54f60d924a4bba179c97d18f7b35b
ErvinDemnushaj <ervin.demnushaj@ryerson.ca>
2022-05-31 01:31:41 -0400

    Update to Room and Container Classes
    Implemented the string building methods we will use for room descriptions and content descriptions.

---
+++ Scripts/assets/items/container.py
@@ -0,0 +1,76 @@
+'''Contains Consumable item class.'''
+
+#Imports
+#Python imports
+#Third party imports
+#Local imports
+from assets.item import Item
+
+class Container(Item):
+    '''Container class'''
+    def __init__(self, name, description, value, weight):
+        super().__init__(name, description, value, weight)
+
+        self._content = []
+        self._open = False
+        self._full_desc = ""
+        self._closed_desc = ""
+        self._empty_desc = ""
+        self._placement = ""
+
+    def __eq__(self, other):
+        return super().__eq__(other)
+    
+    def get_value(self):
+        '''Returns the value of the container plus the values of all items inside'''
+        total_value = self._value
+        for item in self._content:
+            total_value += item._value
+        return total_value
+    
+    def get_weight(self):
+        '''Returns the weight of the container plus the weight of all items inside'''
+        total_weight = self._weight
+        for item in self._content:
+            total_weight += item._weight
+        return total_weight
+
+    def add_item_contents(self, new_item):
+        '''Adds item to the container's contents'''
+        self._content.append(new_item)
+    
+    def remove_item_contents(self, target_item):
+        '''Removes target item from the container's contents'''
+        self._content.remove(target_item)
+    
+    def get_description(self):
+        '''Returns the description of the container along with the placement of the container
+        inside the room. Returns whether the container is full, empty or locked, but
+        does not include individual descriptions of items inside.
+        To be implemented in a room's long description.'''
+        if not(self._open):
+            return self._closed_desc + " " + self._placement 
+        elif len(self._content) == 0:
+            return self._empty_desc + " " + self._placement
+        else:
+            return self._full_desc + " " + self._placement
+    
+    def get_look_description(self):
+        '''Returns the description of the container along with the descriptions of
+        individual items inside if the container is open. To be used when the player looks
+        at the specific container'''
+        if not(self._open):
+            look_desc = "You see " + self._closed_desc + ". "
+        else:
+            if len(self._content) == 0:
+                look_desc = "You see " + self._empty_desc + ". "
+            elif len(self._content) == 1:
+                look_desc = "You see " + self._full_desc + ". " + "It holds " + self._content[0]._description + ". "
+            else:
+                look_desc = "You see " + self._full_desc + ". " + "It holds "
+                for item in range(len(self._content)):
+                    if item != len(self._content) - 1:
+                        look_desc = look_desc + self._content[item]._description + ", "
+                    else:
+                        look_desc = look_desc + "and " + self._content[item]._description + ". "
+        return look_desc
--- Scripts/assets/room.py
+++ Scripts/assets/room.py
@@ -19,13 +19,14 @@ class Room(Asset):
 
     def get_description(self):
         """Returns room's short description."""
-        return "".join(["[", self._name, "]"])
+        short_desc = "".join(["[", self._name, "]"])
+        return " ".join([short_desc, self.build_character_description()])
 
     def get_long_description(self):
         """Returns room's long description."""
-        short_desc = self.get_description()
+        short_desc = "".join(["[", self._name, "]"])
 
-        return "\n".join([short_desc, self._description])
+        return " ".join([short_desc, self._description, self.build_furniture_description(), self.build_character_description(), self.build_item_description()])
 
     def add_item_to_floor(self, new_item):
         '''Adds the specified item to the floor list.'''
@@ -50,3 +51,57 @@ class Room(Asset):
         '''
         connections = monster_connection if self._monster_connections else self._connections
         connections[direction] = room
+    
+    def build_item_description(self): 
+        '''Synthesizes various descriptions of items on the floor into one description.
+        To be used for the long description of a room.'''
+        items_on_floor = self._floor
+        if len(items_on_floor) == 1:
+            add_item_desc = "\n" + "On the floor, you can see " + items_on_floor[0]._description + ". "
+        elif len(items_on_floor) > 1:
+            add_item_desc = "\n" + "On the floor, you can see "
+            for item in range(len(items_on_floor)):
+                if item != len(items_on_floor)-1:
+                    add_item_desc = add_item_desc + items_on_floor[item]._description + ", "
+                else:
+                    add_item_desc = add_item_desc + "and " + items_on_floor[item]._description + ". "
+        else:
+            add_item_desc = ""
+        return add_item_desc
+
+    def build_character_description(self):
+        '''Synthesizes various descriptions of characters in a room into one description.
+        To be used for the long and the short description of a room.'''
+        others_in_room = list(self._characters)
+        if len(others_in_room) == 1:
+            add_char_desc = "\n" + "Inside, you see " + others_in_room[0]._description + ". "
+        elif len(others_in_room) > 1:
+            add_char_desc = "\n" + "Inside, you see "
+            for other in range(len(others_in_room)):
+                if other != len(others_in_room)-1:
+                    add_char_desc = add_char_desc + others_in_room[other]._description + ", "
+                else:
+                    add_char_desc = add_char_desc + "and " + others_in_room[other]._description + ". "
+        else:
+            add_char_desc = ""
+        return add_char_desc
+    
+    def build_furniture_description(self):
+        '''Synthesizes various descriptions of the furniture and their locations in a room into one description.
+        To be used for the long of a room.'''
+        furnishings = self._furniture
+        if len(furnishings) == 1:
+            add_furn_desc = "\n" + "There is a " + furnishings[0].get_description() + ". "
+        elif len(furnishings) > 1:
+            add_furn_desc = "\n" + "There is a "
+            for furniture in range(len(furnishings)):
+                if furniture != len(furnishings)-1:
+                    add_furn_desc = add_furn_desc + furnishings[furniture].get_description() + ", "
+                else:
+                    add_furn_desc = add_furn_desc + "and " + furnishings[furniture].get_description() + ". "
+        else:
+            add_furn_desc = ""
+        return add_furn_desc
+
+
+
