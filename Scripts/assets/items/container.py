4b13d58aadce228ce4986325da80401093b5ec47
Chris-Fontein <104692148+Chris-Fontein@users.noreply.github.com>
2022-06-08 10:47:48 -0400

    Update container.py
    Minor changes to use functions instead of protected attributes.  Removed excess whitespace as per PEP-9

--- Scripts/assets/items/container.py
+++ Scripts/assets/items/container.py
@@ -19,60 +19,55 @@ class Container(Item):
         self._placement = ""
         self._carry_desc = ""
 
-    def __eq__(self, other):
-        return super().__eq__(other)
-    
     def get_value(self):
         '''Returns the value of the container plus the values of all items inside'''
         total_value = self._value
         for item in self._content:
-            total_value += item._value
+            total_value += item.get_value()
         return total_value
-    
+
     def get_weight(self):
         '''Returns the weight of the container plus the weight of all items inside'''
         total_weight = self._weight
         for item in self._content:
-            total_weight += item._weight
+            total_weight += item.get_weight()
         return total_weight
 
     def add_item_contents(self, new_item):
         '''Adds item to the container's contents'''
         self._content.append(new_item)
-    
+
     def remove_item_contents(self, target_item):
         '''Removes target item from the container's contents'''
         self._content.remove(target_item)
-    
+
     def get_furniture_description(self):
         '''Returns the description of the container along with the placement of the container
         inside the room. Returns whether the container is full, empty or locked, but
         does not include individual descriptions of items inside.
         To be implemented in a room's long description.'''
-        if not(self._open):
-            return self._closed_desc + " " + self._placement 
-        elif len(self._content) == 0:
+        if not self._open:
+            return self._closed_desc + " " + self._placement
+        if len(self._content) == 0:
             return self._empty_desc + " " + self._placement
-        else:
-            return self._full_desc + " " + self._placement
-    
+        return self._full_desc + " " + self._placement
+
     def get_container_description(self):
         '''Returns the description of the container along with the descriptions of
         individual items inside if the container is open. To be used when the player looks
         at the specific container'''
-        if not(self._open):
+        if not self._open:
             look_desc = self._closed_desc + ". "
         else:
             if len(self._content) == 0:
                 look_desc = self._empty_desc + ". "
             elif len(self._content) == 1:
-                look_desc = self._full_desc + ". " + self._carry_desc + self._content[0]._description + ". "
+                look_desc = self._full_desc + ". " + self._carry_desc + self._content[0].get_description() + ". "
             else:
                 look_desc = self._full_desc + ". " + self._carry_desc
                 for item in range(len(self._content)):
                     if item != len(self._content) - 1:
-                        look_desc = look_desc + self._content[item]._description + ", "
+                        look_desc = look_desc + self._content[item].get_description() + ", "
                     else:
-                        look_desc = look_desc + "and " + self._content[item]._description + ". "
+                        look_desc = look_desc + "and " + self._content[item].get_description() + ". "
         return look_desc
-    
