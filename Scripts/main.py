f065f2c4247cbedc25d9a82c6933e01e5124bc95
JeesooKim <JeesooKim@users.noreply.github.com>
2022-06-12 17:57:12 -0400

    Make room_builder a Class and call room building in main.

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,4 +1,19 @@
+#import time
+#import yaml
+#import pprint
+from room_builder import Room_builder
 
 if __name__ == "__main__":
-    exec(open("room_builder.py").read())
-    
+    #exec(open("room_builder.py").read())
+
+
+    rooms=Room_builder()
+    game_world=rooms.initialize_room_builder(rooms.read_yaml('characters'), rooms.read_yaml('items'), rooms.read_yaml('rooms'))
+
+
+    print("***** Game Start *****")
+    #print(game_world)
+    characters = game_world['room00']._characters
+    while True:
+        for c in characters:
+            c.action()
--- Scripts/room_builder.py
+++ Scripts/room_builder.py
@@ -1,35 +1,55 @@
 # import pprint
 import yaml
 from assets.characters.adventurer import Adventurer
+from controllers.playerController import PlayerController
+from controllers.adventurerController import AdventurerController
+from controllers.controller import Controller
 from assets.item import Item
 # from assets.items.consumable import Consumable
 from assets.items.container import Container
 from assets.items.equipment import Equipment
 from assets.room import Room
 
+class Room_builder:
 
-def read_yaml(file_name):
-    """ A function to read YAML file"""
-    with open(f"Data/{file_name}.yml") as file:
-        # config = yaml.safe_load(f)
-        data_list = yaml.safe_load(file)
-        # print(roomsList)
-    return data_list  # config
+    def __init__(self):
+        super().__init__()
 
-def build_a_room(room,characters_dict,items_dict):
-    new_room = Room(room['name'], room['description'])
-    for character_obj in room['characters']:
-        # create character
-        # add character to new_r
-        character = characters_dict[character_obj]
-        new_char = Adventurer(
-            character['name'],
-            character['description'],
-            character['identifiers'],
-            character['base_stats'],
-            character['current_health']
-            )
-        new_char.set_room(new_room)
+
+    def read_yaml(self,file_name):
+        """ A function to read YAML file"""
+        with open(f"Data/{file_name}.yml") as file:
+            # config = yaml.safe_load(f)
+            data_list = yaml.safe_load(file)
+            # print(roomsList)
+        return data_list  # config
+
+    def build_a_room(self,room,characters_dict,items_dict):
+        new_room = Room(room['name'], room['description'])
+        for character_obj in room['characters']:
+            # create character
+            # add character to new_r
+            character = characters_dict[character_obj]
+            new_char = Adventurer(
+                character['name'],
+                character['description'],
+                character['identifiers'],
+                character['base_stats'],
+                character['current_health']
+                )
+            new_char.set_room(new_room)
+
+            if character['controller'] == 'AdventurerController':
+                new_char.set_controller(AdventurerController())
+
+            elif character['controller'] == 'PlayerController':
+                new_char.set_controller(PlayerController())
+
+            #elif character['controller'] == 'MonsterController':
+                #new_char.set_controller(MonsterController())
+            else:
+                new_char.set_controller(Controller())
+
 
         if room['floor'] is not None:
             for floor_obj in room['floor']:
@@ -76,50 +96,51 @@ def build_a_room(room,characters_dict,items_dict):
                     )
                 new_room.add_item_to_floor(new_items_on_floor)
 
-        if room['furniture'] is not None:
-            for furniture_obj in room['furniture']:
-                # create furniture
-                # add items to furniture
-                # add furniture to new_r
-                furniture = items_dict[list(furniture_obj.keys())[0]]
-                new_furniture = Container(
-                    furniture['name'],
-                    furniture['description'],
-                    furniture['identifiers'],
-                    furniture['value'],
-                    furniture['weight'],
-                    furniture['open'])
-                new_room.add_funiture(new_furniture)
-    return new_room
+            if room['furniture'] is not None:
+                for furniture_obj in room['furniture']:
+                    # create furniture
+                    # add items to furniture
+                    # add furniture to new_r
+                    furniture = items_dict[list(furniture_obj.keys())[0]]
+                    new_furniture = Container(
+                        furniture['name'],
+                        furniture['description'],
+                        furniture['identifiers'],
+                        furniture['value'],
+                        furniture['weight'],
+                        furniture['open'])
+                    new_room.add_funiture(new_furniture)
+        return new_room
 
 
-def initialize_room_builder(yaml_characters,yaml_items,yaml_rooms):
+    def initialize_room_builder(self, yaml_characters,yaml_items,yaml_rooms):
 
-    rooms = {} #[]
-    room_dict = yaml_rooms
+        rooms = {} #[]
+        room_dict = yaml_rooms
 
-    for room_id in room_dict:
-        room =room_dict[room_id]
-        new_room = build_a_room(room,yaml_characters,yaml_items)
-        rooms[room_id] = new_room
-        #rooms.append(new_room)
+        for room_id in room_dict:
+            room =room_dict[room_id]
+            new_room = self.build_a_room(room,yaml_characters,yaml_items)
+            rooms[room_id] = new_room
+            #rooms.append(new_room)
 
-    #Now, the connections will be established below based on all the rooms built above.
-    for room_id in room_dict:
-        room=rooms[room_id]
-        if room_dict[room_id]['connections'] is not None:
-            for connection_obj in room_dict[room_id]['connections']:
-                connection_direction = connection_obj[0]
-                connected_room_id = connection_obj[1]
-                room.add_room_connection(connection_direction, connected_room_id,False)
+        #Now, the connections will be established below based on all the rooms built above.
+        for room_id in room_dict:
+            room=rooms[room_id]
+            if room_dict[room_id]['connections'] is not None:
+                for connection_obj in room_dict[room_id]['connections']:
+                    connection_direction = connection_obj[0]
+                    connected_room_id = connection_obj[1]
+                    room.add_room_connection(connection_direction, connected_room_id,False)
 
-        if room_dict[room_id]['monster_connections'] is not None:
-            for connection_obj in room_dict[room_id]['monster_connections']:
-                connection_direction = connection_obj[0]
-                connected_room_id = connection_obj[1]
-                room.add_room_connection(connection_direction, connected_room_id,True)
+            if room_dict[room_id]['monster_connections'] is not None:
+                for connection_obj in room_dict[room_id]['monster_connections']:
+                    connection_direction = connection_obj[0]
+                    connected_room_id = connection_obj[1]
+                    room.add_room_connection(connection_direction, connected_room_id,True)
 
-    #rooms
+        return rooms
 
 
-initialize_room_builder(read_yaml('characters'),read_yaml('items'),read_yaml('rooms'))
+
+    #initialize_room_builder(read_yaml('characters'),read_yaml('items'),read_yaml('rooms'))
